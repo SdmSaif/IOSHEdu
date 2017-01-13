a0cc336bfcddf36ead7c43e0f5fdf9ffdd91f17c
Malak <malak-alsaif@malaks-macbook-pro.local>
2017-01-13 23:50:43 +0300

    Initial Commit

---
+++ EducationHelper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EducationHelper
+//
+//  Created by Malak on 4/15/1438 AH.
+//  Copyright © 1438 Malak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
